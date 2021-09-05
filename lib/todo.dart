acf9d280c2635fe2f3de4f5db43364e31beedcd8
worktree28 <hmishra2001@live.in>
2021-09-05 21:40:42 +0530

    implemented edit on swipe

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -10,7 +9,7 @@ class TodoList extends StatefulWidget {
 
 class _TodoListState extends State<TodoList> {
   // save data
-  final List<String> _todoList = <String>[];
+  //final List<String> _todoList = <String>[];
   // text field
   final TextEditingController _textFieldController = TextEditingController();
   final listToDo = FirebaseFirestore.instance.collection('hello');
@@ -19,20 +18,50 @@ class _TodoListState extends State<TodoList> {
     return Scaffold(
       appBar: AppBar(title: const Text('To-Do List')),
       body: StreamBuilder<QuerySnapshot>(
-        stream: listToDo.snapshots(),
+        stream:
+            listToDo.orderBy('time_inserted', descending: false).snapshots(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.hasError) return Text('Error: ${snapshot.error}');
 
           if (snapshot.connectionState == ConnectionState.waiting)
-            return (Text('loading...'));
+            return (Center(
+              child: CircularProgressIndicator(),
+            ));
           return ListView(
             children: snapshot.data!.docs.map((DocumentSnapshot document) {
               Map<String, dynamic> data =
                   document.data()! as Map<String, dynamic>;
               //print(document.id);
-              return ListTile(
-                title: Text(data['data']),
-                onTap: () => _promptRemoveTodoItem(data['data'], document.id),
+              return Dismissible(
+                key: Key(document.id),
+                child: ListTile(
+                  title: Text(data['data']),
+                ),
+                background: Container(
+                  color: Colors.red,
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                  ),
+                ),
+                secondaryBackground: Container(
+                  color: Colors.amber,
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  alignment: AlignmentDirectional.centerEnd,
+                  child: Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                  ),
+                ),
+                onDismissed: (DismissDirection direction) {
+                  if (direction == DismissDirection.startToEnd)
+                    _removeTodoItem(document.id);
+                  else
+                    _editTodoItem(
+                        data['data'], document.id, data['time_inserted']);
+                },
               );
             }).toList(),
           );
@@ -48,70 +77,75 @@ class _TodoListState extends State<TodoList> {
   }
 
   Future<void> _addTodoItem(String title) {
-    return listToDo.add({'data': title});
-    
+    return listToDo
+        .add({'data': title, 'time_inserted': FieldValue.serverTimestamp()});
   }
 
-void _promptRemoveTodoItem(String data, String id) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return new AlertDialog(
-        title: new Text('Mark $data as done?'),
-        actions: <Widget>[
-          new TextButton(
-            child: new Text('CANCEL'),
-            onPressed: () => Navigator.of(context).pop()
-          ),
-          new TextButton(
-            child: new Text('MARK AS DONE'),
-            onPressed: () {
-              _removeTodoItem(id);
-              Navigator.of(context).pop();
-            }
-          )
-        ]
-      );
-    }
-  );
-}
+  Future<void> _edit(String update, String id, Timestamp time) {
+    _removeTodoItem(id);
+    return listToDo.add({'data': update, 'time_inserted': time});
+  }
 
+  void _editTodoItem(String data, String id, Timestamp time) {
+    _textFieldController.text = data;
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return new AlertDialog(
+          title: new Text('Edit $data'),
+          content: TextField(
+            controller: _textFieldController,
+            decoration: const InputDecoration(hintText: "type here"),
+          ),
+          actions: <Widget>[
+            new TextButton(
+              child: new Text('Edit'),
+              onPressed: () {
+                _edit(_textFieldController.text, id, time);
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
 
   Future<void> _removeTodoItem(String id) {
     return listToDo.doc(id).delete();
   }
 
-  
   // display a dialog for the user to enter items
   Future<dynamic> _displayDialog(BuildContext context) async {
     return showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Add a task to your list'),
-            content: TextField(
-              controller: _textFieldController,
-              decoration: const InputDecoration(hintText: 'Enter task here'),
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add a task to your list'),
+          content: TextField(
+            controller: _textFieldController,
+            decoration: const InputDecoration(hintText: 'Enter task here'),
+          ),
+          actions: <Widget>[
+            // add button
+            TextButton(
+              child: const Text('ADD'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _addTodoItem(_textFieldController.text);
+              },
             ),
-            actions: <Widget>[
-              // add button
-              TextButton(
-                child: const Text('ADD'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  _addTodoItem(_textFieldController.text);
-                },
-              ),
-              // Cancel button
-              TextButton(
-                child: const Text('CANCEL'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              )
-            ],
-          );
-        });
+            // Cancel button
+            TextButton(
+              child: const Text('CANCEL'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
   }
-
 }
